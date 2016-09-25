0df5d12ac0b87acd9a71f36f83f03eb66676b82a
Giacomo DR <delrig@usi.ch>
2016-09-25 20:35:28 +0200

    Almost done

--- src/ch/usi/delrig/JarAnalyzer.java
+++ src/ch/usi/delrig/JarAnalyzer.java
@@ -8,6 +8,7 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.MethodNode;
 
@@ -15,30 +16,77 @@ import org.objectweb.asm.tree.MethodNode;
 public class JarAnalyzer {
 
   public static void main(final String[] args) throws IOException {
-    final String jarFileName = args[0]; 
-    System.out.println("Analyzing "+jarFileName);
-    final JarFile jar = new JarFile(jarFileName);
-    final Enumeration<JarEntry> entries = jar.entries();
-    while (entries.hasMoreElements()) {
-      final JarEntry entry = entries.nextElement();
-      if (!entry.isDirectory() && entry.getName().endsWith(".class")) {
-        System.out.println(entry.getName());
-        final InputStream is = jar.getInputStream(entry);
-        final ClassReader classReader = new ClassReader(is);
-        final ClassNode classNode = new ClassNode();
-        classReader.accept(classNode, ClassReader.SKIP_FRAMES);
-        System.out.println("  Class "+classNode.name);                          
-        final List<MethodNode> methods = classNode.methods;
-        for (final MethodNode methodNode : methods) {
-          System.out.println("    Method "+methodNode.name+methodNode.desc);
-          //...
-        }
-      }
-    }
+	final String jarFileName = args[0]; 
+    final JarFile jar = new JarFile( jarFileName );
+    System.out.println( "Analyzing " + jar.getName() );
+    analyzeJar( jar );
+    
+    System.out.println( "Results" );
+    //TODO Show statistics
   }
   
-  private static void analyzeJar( JarFile jar ){
+  private static JarStatistics analyzeJar( JarFile jar ) throws IOException{
+	  JarStatistics stats = new JarStatistics();
+	  stats.jarFilename = jar.getName();
 	  
+	  final Enumeration<JarEntry> entries = jar.entries();
+	  while( entries.hasMoreElements() ){
+		  final JarEntry entry = entries.nextElement();
+		  if( !entry.isDirectory() && entry.getName().endsWith(".class") ){
+			  final InputStream is = jar.getInputStream( entry );
+			  final ClassReader classReader = new ClassReader( is );
+			  analyzeClass( classReader, stats );
+	      }
+	  }
+	  
+	  return stats;
+  }
+  
+  private static void analyzeClass( ClassReader classReader, JarStatistics stats ){
+	  final ClassNode classNode = new ClassNode();
+      classReader.accept( classNode, ClassReader.SKIP_FRAMES );
+      System.out.println( "  Class " + classNode.name );
+	  stats.numberOfClasses++;
+      
+      @SuppressWarnings("unchecked")
+	  final List<MethodNode> methods = classNode.methods;
+      for( final MethodNode methodNode : methods ){
+    	  analyzeMethod( methodNode, stats );
+      }
+  }
+  
+  private static void analyzeMethod( MethodNode method, JarStatistics stats ){
+	  if( method.instructions.size() == 0 )
+		  return; // Skip empty methods
+	  
+	  System.out.println("    Method " + method.name + method.desc );
+	  stats.numberOfMethodsWithCode++;
+	  
+	  AbstractInsnNode inst = method.instructions.getFirst();
+	  while( inst != null ){
+		  int opcode = inst.getOpcode();
+		  if( opcode != -1 ){
+			  stats.numberOfInstructions++;
+			  stats.numberOfInstructionsByOpcode[opcode]++;
+			  
+			  if( isBranching(opcode) )
+				  stats.numberOfBranchInvocations++;
+			  
+			  if( isInvocation(opcode) )
+				  stats.numberOfMethodInvocations++;
+		  }
+		  inst = inst.getNext();
+	  }
+  }
+  
+  static boolean isBranching( int opcode ){
+	  // TODO Check whether opcode is a branch or not excl. goto
+	  return false;
+  }
+  
+  static boolean isInvocation( int opcode ){
+	  // TODO Check whether opcode is method call
+	  return false;
   }
 
 }
--- src/ch/usi/delrig/JarStatistics.java
+++ src/ch/usi/delrig/JarStatistics.java
@@ -1,9 +1,11 @@
 package ch.usi.delrig;
 
 public class JarStatistics {
-
-	public String jarFilename;
-	public int numberOfClasses;
-	public int numberOfMethodsWithCode;
-	public int numberOfInstructions;
+	public String jarFilename = "";
+	public int numberOfClasses = 0;
+	public int numberOfMethodsWithCode = 0;
+	public int numberOfInstructions = 0;
+	public int[] numberOfInstructionsByOpcode = new int[256];
+	public int numberOfMethodInvocations = 0;
+	public int numberOfBranchInvocations = 0;
 }

f900fb054e1b1949ad60311bfc2cd157e65a3720
Giacomo DR <delrig@usi.ch>
2016-09-27 08:39:20 +0200

    Complete version

--- src/ch/usi/delrig/JarAnalyzer.java
+++ src/ch/usi/delrig/JarAnalyzer.java
@@ -6,23 +6,43 @@ import java.util.Enumeration;
 import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.stream.IntStream;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.util.Printer;
 
 
 public class JarAnalyzer {
+	private static int[] invokeInstructions = {
+			0xBA, 0xB9, 0xB7, 0xB8, 0xB6
+	};
+	
+	private static int[] branchInstructions = {
+			0xA5, 0xA6, 0x9F, 0xA2, 0xA3, 0xA4,
+			0xA1, 0xA0, 0x99, 0x9C, 0x9D, 0x9E, 
+			0x9B, 0x9A, 0xC7, 0xC6, 0x94, 0x96, 
+			0x95, 0x98, 0x97, 0xC1 /*NOT SURE*/,
+			0xA8, 0xC9
+	};
 
   public static void main(final String[] args) throws IOException {
-	final String jarFileName = args[0]; 
-    final JarFile jar = new JarFile( jarFileName );
-    System.out.println( "Analyzing " + jar.getName() );
-    analyzeJar( jar );
+	  
+    if(args.length == 0) {
+    	System.out.println("No input file.");
+    	System.exit(1);
+    }
     
-    System.out.println( "Results" );
-    //TODO Show statistics
+    final String jarFileName = args[0]; 
+    final JarFile jar = new JarFile( jarFileName );
+    
+//    System.out.println( "Analyzing " + jar.getName() );
+    
+    
+    System.out.println( "Results\n" );
+    System.out.println(analyzeJar(jar));
   }
   
   private static JarStatistics analyzeJar( JarFile jar ) throws IOException{
@@ -45,7 +65,7 @@ public class JarAnalyzer {
   private static void analyzeClass( ClassReader classReader, JarStatistics stats ){
 	  final ClassNode classNode = new ClassNode();
       classReader.accept( classNode, ClassReader.SKIP_FRAMES );
-      System.out.println( "  Class " + classNode.name );
+//      System.out.println( "  Class " + classNode.name );
 	  stats.numberOfClasses++;
       
       @SuppressWarnings("unchecked")
@@ -59,7 +79,7 @@ public class JarAnalyzer {
 	  if( method.instructions.size() == 0 )
 		  return; // Skip empty methods
 	  
-	  System.out.println("    Method " + method.name + method.desc );
+//	  System.out.println("    Method " + method.name + method.desc );
 	  stats.numberOfMethodsWithCode++;
 	  
 	  AbstractInsnNode inst = method.instructions.getFirst();
@@ -70,7 +90,7 @@ public class JarAnalyzer {
 			  stats.numberOfInstructionsByOpcode[opcode]++;
 			  
 			  if( isBranching(opcode) )
-				  stats.numberOfBranchInvocations++;
+				  stats.numberOfBranchInstructions++;
 			  
 			  if( isInvocation(opcode) )
 				  stats.numberOfMethodInvocations++;
@@ -80,13 +100,11 @@ public class JarAnalyzer {
   }
   
   static boolean isBranching( int opcode ){
-	  // TODO Check whether opcode is a branch or not excl. goto
-	  return false;
+	  return IntStream.of(branchInstructions).anyMatch(code -> code == opcode);
   }
   
   static boolean isInvocation( int opcode ){
-	  // TODO Check whether opcode is method call
-	  return false;
+	  return IntStream.of(invokeInstructions).anyMatch(code -> code == opcode);
   }
 
 }
--- src/ch/usi/delrig/JarStatistics.java
+++ src/ch/usi/delrig/JarStatistics.java
@@ -1,11 +1,37 @@
 package ch.usi.delrig;
 
+import org.objectweb.asm.util.Printer;
+
 public class JarStatistics {
 	public String jarFilename = "";
 	public int numberOfClasses = 0;
 	public int numberOfMethodsWithCode = 0;
 	public int numberOfInstructions = 0;
-	public int[] numberOfInstructionsByOpcode = new int[256];
+	public int[] numberOfInstructionsByOpcode = new int[200];
 	public int numberOfMethodInvocations = 0;
-	public int numberOfBranchInvocations = 0;
+	public int numberOfBranchInstructions = 0;
+	
+	@Override
+	public String toString() {
+		return  "File name: \t\t\t\t" + jarFilename + '\n' +
+				"Number of classes: \t\t\t" + Integer.toString(numberOfClasses) + "\n" +
+				"Number of methods with code: \t\t" + Integer.toString(numberOfMethodsWithCode) + "\n" +
+				"Number of method invocations: \t\t" + Integer.toString(numberOfMethodInvocations) + "\n" +
+				"Number of instructions: \t\t" + Integer.toString(numberOfInstructions) + "\n" +
+				"Number of branch invocations: \t\t" + Integer.toString(numberOfBranchInstructions) + "\n" +
+				"Number of instructions by opcode:\n" + printNumberOfInstructionsByOpcode();
+				
+	}
+	
+	private String printNumberOfInstructionsByOpcode() {
+		String result = ""; 
+		for(int i = 0; i < numberOfInstructionsByOpcode.length; i++) {
+			String opCodeName = Printer.OPCODES[i];
+			if(!opCodeName.equals("")) {
+				result += "\t" + opCodeName + ": " + Integer.toString(numberOfInstructionsByOpcode[i]) + "\n";
+			}
+			
+		}
+		return result;
+	}
 }
